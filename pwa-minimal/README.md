0b081f42dc7a618f055af046bcc7d7c8a826f699
kmnr84 <kmnr84@gmail.com>
2024-07-05 13:03:20 +0900

    feat: add files

--- pwa-minimal/README.md
+++ pwa-minimal/README.md
@@ -1,1 +1,12 @@
 # PWA-MINIMAL
+
+- index.html
+- icon.jpg
+- app.webmanifest
+- serviceworker.js
+
+## Run Locally
+
+```
+npx http-server
+```
---
+++ pwa-minimal/public/serviceworker.js
@@ -0,0 +1,5 @@
+self.addEventListener('fetch', event => {});
+self.addEventListener('activate', event => {});
+self.addEventListener('install', event => {});
+self.addEventListener('push', event => {});
+self.addEventListener('notificationclick', event => {});
